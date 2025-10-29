010b03db4df75658cf49dd24a43f89937417d0e4
transformative <108471888+zouun@users.noreply.github.com>
2025-10-30 00:41:58 +0800

    Add files via upload

--- src/config/path_config.py
+++ src/config/path_config.py
@@ -5,12 +5,7 @@ from os import path
 
 
 def sanitize_filename(filename: str) -> str:
-    """
-    去除文件名中的非法字符，并返回新的文件名
-    要注意, 返回的可能是空字符串
-    """
-
-    invalid_chars = r'[<>:"/\\|?*]'
+    invalid_chars = r'[<>:"/\\|?*\n]'
     sanitized_filename = re.sub(invalid_chars, "", filename)
     return sanitized_filename
 
